c33a76d56c31f06d9123d2b734d8e22caff3796e
Cristian David Herrera <dh99583@gmail.com>
2022-07-01 08:42:27 -0500

    Update Rol and validation for Update User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,71 +20,9 @@ class UserController extends Controller
         $this->middleware('can:user')->only('getUser');
         $this->middleware('can:updateUser')->only('update');
         $this->middleware('can:deleteUser')->only('delete');
-        $this->middleware('can:logoutUser')->only('logout');
         $this->middleware('can:showAllUser')->only('index');
     }
-    /**
-    * @param  \Illuminate\Http\Request  $request
-    * @param $credentials para luego validarse si en la base hay un usuario que cumpla con esas credenciales
-    *        Si se logra autentificar nos devuelte el token
-    *        Si no, devuelve el estatus
-    *
-    * @return Response  json(token) o status
-    */
-    public function login(Request $request){
 
-    $credentials = $request->only('email', 'password');
-    try {
-            if (! $token = JWTAuth::attempt($credentials)) {
-
-                return response()->json(['error' => 'invalid_credentials'], 400);
-
-            }
-        } catch (JWTException $e) {
-
-            return response()->json(['error' => 'could_not_create_token'], 500);
-
-        }
-
-    return response()->json(compact('token'));
-    }
-
-    /**
-     * @param  \Illuminate\Http\Request  $request
-     * Process     @param $validator =Validator::make($request->all()  Valida que los datos recibidos si cumplan  // si la validacion falla devuelve un 400
-     * Process     @param $user = User::create  Crea el Usuario y encripta la Password con el Hash
-     * Process     @param $token = JWTAuth::fromUser($user); Obtenemos el token correspondiente a ese usuario creado
-     *
-     * @return Response json(compact('user','token'),201)
-     */
-
-    public function register(Request $request){
-
-        $validator = Validator::make($request->all(), [
-            'document'=>'required|integer|min:10',
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-
-            if($validator->fails()){
-
-            return response()->json($validator->errors()->toJson(), 400);
-
-            }
-
-        $user = User::create([
-            'document' => $request->get('document'),
-            'name' => $request->get('name'),
-            'email' => $request->get('email'),
-            'password' => Hash::make($request->get('password')),
-        ])->assignRole('custumer');
-
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json(compact('user','token'),201);
-
-    }
 
     /**
      * @param User $user
@@ -122,30 +60,6 @@ class UserController extends Controller
     }
 
     /**
-     * Invalida el token JWT del usuario para finalizar la sesion
-     * En el caso de que salga error se le indica al usuario try again
-     *
-     * @return response ->json();
-     */
-    public function logout(){
-
-        try {
-            JWTAuth::invalidate(JWTAuth::getToken());
-            return response()->json([
-              'status' => 'success',
-              'message' => 'You have successfully logged out.'
-            ]);
-        } catch (JWTException $e) {
-              JWTAuth::unsetToken();
-              // algo salió mal tratando de validar un token inválido
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Failed to logout, please try again.'
-                ]);
-            }
-    }
-
-    /**
      * Eliminar Usuario
      *
      * @param $id
@@ -166,6 +80,18 @@ class UserController extends Controller
      */
 
     public function update(Request $request){
+        $validator = Validator::make($request->all(), [
+            'document'=>'integer|min:10',
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255|unique:users',
+            'password' => 'string|min:6|confirmed',
+        ]);
+
+            if($validator->fails()){
+
+            return response()->json($validator->errors()->toJson(), 400);
+
+            }
         $user = Auth::user();
 
         if ( ! $request->get('document') == ''){
